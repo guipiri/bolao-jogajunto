5ff2bfdd01c1676b2c76b39bfa333b64da6b2bcf
guipiri <gui.soliveiras@gmail.com>
2023-08-27 22:24:44 -0300

    feat: adjusting pages min height, footer got 3rem of margin top

--- src/routes/regras/Regras.jsx
+++ src/routes/regras/Regras.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Regras.css";
 
 function Regras() {
-  return <div style={{ minHeight: "calc(100vh - 221px)" }}>Regras</div>;
+  return <div style={{ minHeight: "calc(100vh - 221px - 3rem)" }}>Regras</div>;
 }
 
 export default Regras;
