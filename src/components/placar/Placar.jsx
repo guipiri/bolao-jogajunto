0e087757056fdf270cee7c3270cd6dfb011dac97
guipiri <gui.soliveiras@gmail.com>
2023-08-27 22:07:11 -0300

    feat: finishing placar component

--- src/components/placar/Placar.jsx
+++ src/components/placar/Placar.jsx
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Placar.css";
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
+import { BiSolidTShirt } from "react-icons/bi";
 
 function Placar() {
-  return <div>Placar</div>;
+  const [score, setScore] = useState({ A: 0, B: 0 });
+  const plusOne = (team) => {
+    if (team === "A") {
+      setScore({ ...score, A: score.A + 1 });
+    } else {
+      setScore({ ...score, B: score.B + 1 });
+    }
+  };
+  const minusOne = (team) => {
+    if (team === "A") {
+      if (score.A != 0) {
+        setScore({ ...score, A: score.A - 1 });
+      }
+    } else {
+      if (score.B != 0) {
+        setScore({ ...score, B: score.B - 1 });
+      }
+    }
+  };
+  return (
+    <div className="placar">
+      <div>
+        <div className="teamCircle">
+          <BiSolidTShirt />
+        </div>
+        <span>Time A</span>
+        <div className="modifyScore">
+          <AiOutlineMinusCircle onClick={() => minusOne("A")} />
+          <AiOutlinePlusCircle onClick={() => plusOne("A")} />
+        </div>
+      </div>
+      <h3>
+        {score.A} x {score.B}
+      </h3>
+      <div>
+        <div className="teamCircle">
+          <BiSolidTShirt />
+        </div>
+        <span>Time B</span>
+        <div className="modifyScore">
+          <AiOutlineMinusCircle onClick={() => minusOne("B")} />
+          <AiOutlinePlusCircle onClick={() => plusOne("B")} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Placar;
--- src/routes/palpite/Palpite.jsx
+++ src/routes/palpite/Palpite.jsx
@@ -1,35 +1,20 @@
 import React, { useState } from "react";
 import "./Palpite.css";
 import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
-function Palpite() {
-  const [score, setScore] = useState({ A1: 0, B1: 0 });
+import { BiSolidTShirt } from "react-icons/bi";
+import Placar from "../../components/placar/Placar";
 
+function Palpite() {
   return (
     <div className="palpite">
       <div className="palpitesDesc">
         <h3>Bolão Joga Junto</h3>
         <p>Rodada semana do consumidor</p>
-        <div className="placar">
-          <div>
-            <div className="teamCircle"></div>
-            <span>Time A</span>
-            <div className="modifyScore">
-              <AiOutlineMinusCircle style={{ color: "red" }} />
-              <AiOutlinePlusCircle  />
-            </div>
-          </div>
-          <h3>
-            {score.A1} x {score.B1}
-          </h3>
-          <div>
-            <div className="teamCircle"></div>
-            <span>Time B</span>
-            <div className="modifyScore">
-              <AiOutlineMinusCircle style={{ color: "red" }} />
-              <AiOutlinePlusCircle  />
-            </div>
-          </div>
-        </div>
+        <Placar />
+        <Placar />
+        <Placar />
+        <Placar />
+        <Placar />
       </div>
     </div>
   );
