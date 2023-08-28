45e5e5203e1a06a6e960c869ec28bdaccaa4fc4a
guipiri <gui.soliveiras@gmail.com>
2023-08-27 21:21:14 -0300

    feat: starting placar component

---
+++ src/components/placar/Placar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Placar.css";
+
+function Placar() {
+  return <div>Placar</div>;
+}
+
+export default Placar;
--- src/routes/palpite/Palpite.jsx
+++ src/routes/palpite/Palpite.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Palpite.css";
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
 function Palpite() {
-  return <div style={{ minHeight: "calc(100vh - 221px)" }}>Palpite</div>;
+  const [score, setScore] = useState({ A1: 0, B1: 0 });
+
+  return (
+    <div className="palpite">
+      <div className="palpitesDesc">
+        <h3>Bolão Joga Junto</h3>
+        <p>Rodada semana do consumidor</p>
+        <div className="placar">
+          <div>
+            <div className="teamCircle"></div>
+            <span>Time A</span>
+            <div className="modifyScore">
+              <AiOutlineMinusCircle style={{ color: "red" }} />
+              <AiOutlinePlusCircle  />
+            </div>
+          </div>
+          <h3>
+            {score.A1} x {score.B1}
+          </h3>
+          <div>
+            <div className="teamCircle"></div>
+            <span>Time B</span>
+            <div className="modifyScore">
+              <AiOutlineMinusCircle style={{ color: "red" }} />
+              <AiOutlinePlusCircle  />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Palpite;
