f20b66316f8b1f380a44ab252e91f8489ca0b41a
guipiri <gui.soliveiras@gmail.com>
2023-08-30 17:08:43 -0300

    feat: adding loader to palpites in 2 differents steps 1.when fetching scores already made 2.submiting new scores

--- src/routes/palpite/Palpite.jsx
+++ src/routes/palpite/Palpite.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import "./Palpite.css";
 import Placar from "../../components/placar/Placar";
 import { UserContext } from "../../context/UserContext";
+import Loader from "../../components/loader/Loader";
 
 function Palpite() {
   const matches = [
@@ -21,6 +22,7 @@ function Palpite() {
     "https://script.google.com/macros/s/AKfycbxFhcy77WfGk3iFhZJsh5Fsqxfq7CLAFuuvsiz-P35XyLi4_CArOq0IeR4T2AZLhaI/exec";
 
   const [isChangeable, setIsChangeable] = useState(false);
+  const [loaderOn, setLoaderOn] = useState(false);
   const [scores, setScores] = useState([
     [0, 0],
     [0, 0],
@@ -60,6 +62,7 @@ function Palpite() {
 
   useEffect(() => {
     if (userId) {
+      setLoaderOn(true);
       fetch(`${url}?id=${userId}`, {
         method: "GET",
         headers: {
@@ -85,6 +88,9 @@ function Palpite() {
         })
         .catch((err) => {
           console.log(err);
+        })
+        .finally(() => {
+          setLoaderOn(false);
         });
     }
   }, []);
@@ -93,6 +99,8 @@ function Palpite() {
     const body = [...scores];
     body.pop();
     body.pop();
+    setLoaderOn(true);
+    console.log(loaderOn);
     fetch(`${url}?id=${userId}`, {
       method: "POST",
       body: JSON.stringify(body),
@@ -107,56 +115,62 @@ function Palpite() {
         return res.json();
       })
       .then((data) => console.log(data))
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err))
+      .finally(() => {
+        setLoaderOn(false);
+      });
   };
   return (
-    <div className="flexColumnCenter">
-      <div className="palpites flexColumnCenter">
-        <div className="palpitesHeader">
-          <div>
-            <h3>Bolão Joga Junto</h3>
-            <p>Qual será a pontuação correta?</p>
+    <>
+      <div className="flexColumnCenter">
+        <div className="palpites flexColumnCenter">
+          <div className="palpitesHeader">
+            <div>
+              <h3>Bolão Joga Junto</h3>
+              <p>Qual será a pontuação correta?</p>
+            </div>
+            <button
+              className={isChangeable ? "none" : "button"}
+              onClick={handleEdit}
+            >
+              Editar palpites
+            </button>
           </div>
+          {matches.map((element, index) => {
+            if (index < 10) {
+              return (
+                <Placar
+                  key={`partida${index + 1}`}
+                  aTeamName={element[0]}
+                  bTeamName={element[1]}
+                  arrayScore={element}
+                  isChangeable={isChangeable}
+                  scores={scores}
+                  setScores={setScores}
+                  index={index}
+                />
+              );
+            }
+          })}
+        </div>
+        <div className="flexColumnCenter divButton">
           <button
-            className={isChangeable ? "none" : "button"}
+            onClick={handleSubmit}
+            type="submit"
+            className={!isChangeable ? "none" : "button"}
+          >
+            Enviar
+          </button>
+          <button
+            className={isChangeable ? "none" : "button above"}
             onClick={handleEdit}
           >
             Editar palpites
           </button>
         </div>
-        {matches.map((element, index) => {
-          if (index < 10) {
-            return (
-              <Placar
-                key={`partida${index + 1}`}
-                aTeamName={element[0]}
-                bTeamName={element[1]}
-                arrayScore={element}
-                isChangeable={isChangeable}
-                scores={scores}
-                setScores={setScores}
-                index={index}
-              />
-            );
-          }
-        })}
       </div>
-      <div className="flexColumnCenter divButton">
-        <button
-          onClick={handleSubmit}
-          type="submit"
-          className={!isChangeable ? "none" : "button"}
-        >
-          Enviar
-        </button>
-        <button
-          className={isChangeable ? "none" : "button above"}
-          onClick={handleEdit}
-        >
-          Editar palpites
-        </button>
-      </div>
-    </div>
+      <Loader on={loaderOn} />
+    </>
   );
 }
 
